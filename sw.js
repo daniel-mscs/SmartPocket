e6eb05170b4d61528657e29150e5cd5b0f6bd166
Daniel <daniel.mth1996@gmail.com>
2026-03-15 21:44:14 -0300

    fix: update cache version + reset seguro

--- sw.js
+++ sw.js
@@ -1,4 +1,6 @@
-const CACHE_NAME = 'smartpocket-v1';
+// sw.js
+const CACHE_NAME = 'smartpocket-v2';
+
 const assets = [
   './',
   './index.html',
@@ -8,14 +10,23 @@ const assets = [
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  // limpa caches antigos
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
